895170baf7f014c9e1836392be7f477d7cb38783
Julianto Chai <saijnawazaki@gmail.com>
2026-08-26 14:31:36 +0700

    pisahkan test

--- index.php
+++ index.php
@@ -64,16 +64,5 @@ ksort($data);
 
 //print_r($data);
 file_put_contents('mizuki.min.css', $data);
-?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Mizuki</title>
-    <link href="mizuki.min.css?time=<?=time()?>" rel="stylesheet" crossorigin="anonymous">
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
-  </body>
-</html>
+
+echo 'Generated at '.date('r');
